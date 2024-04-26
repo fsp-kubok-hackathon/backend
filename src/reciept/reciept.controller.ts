f935cd5bd9ff8fc83ad7aa82e243fdf860562159
Dmitry Evteev <haxrelt@gmail.com>
2024-04-26 19:14:18 +0300

    fix docker

--- src/reciept/reciept.controller.ts
+++ src/reciept/reciept.controller.ts
@@ -4,6 +4,7 @@ import {
   Get,
   UseInterceptors,
   UploadedFiles,
+  Logger,
 } from '@nestjs/common';
 import { RecieptService } from './reciept.service';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
@@ -17,6 +18,8 @@ import { UserReciept } from './dto/reciept.dto';
 @ApiTags('Чеки')
 @Controller('reciepts')
 export class RecieptController {
+  private readonly logger = new Logger('RecieptController');
+
   constructor(private readonly service: RecieptService) {}
 
   @Get('/')
