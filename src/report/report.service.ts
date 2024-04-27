d3f0f0572502daa93fed08fd45049f1aed15ea41
Arteandr <artedamatik@gmail.com>
2024-04-27 13:07:15 +0300

    add get method

--- src/report/report.service.ts
+++ src/report/report.service.ts
@@ -5,7 +5,6 @@ import Pagination from 'src/shared/pagination';
 import { ReportFilters } from './entities/report.entity';
 import { readToExcel, streamFromUrl } from 'src/utils/excel';
 import { MinioService } from 'src/minio/minio.service';
-import { isEqual } from 'date-fns';
 
 @Injectable()
 export class ReportService {
@@ -59,6 +58,24 @@ export class ReportService {
     });
 
     return this.validate(ticketId);
+  }
+
+  async getReportItems(ticketId: string, approved: boolean) {
+    return this.prisma.$transaction(async (tx) => {
+      const ticket = await tx.ticket.findFirstOrThrow({
+        where: { id: ticketId },
+        include: { report: true },
+      });
+
+      if (!ticket || !ticket.report)
+        throw new NotFoundException('Тикет не найден');
+
+      const items = await tx.reportItem.findMany({
+        where: { AND: [{ reportId: ticket.report.id }, { approved }] },
+      });
+
+      return items;
+    });
   }
 
   async validate(ticketId: string) {
