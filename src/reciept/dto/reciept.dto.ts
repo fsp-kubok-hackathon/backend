a9e86ebedd8ecf9572cd24e58fc95003f0881491
Arteandr <artedamatik@gmail.com>
2024-04-27 16:35:24 +0300

    link categories to reciepts

--- src/reciept/dto/reciept.dto.ts
+++ src/reciept/dto/reciept.dto.ts
@@ -3,18 +3,25 @@ import { ApiProperty } from '@nestjs/swagger';
 export class UserReciept {
   @ApiProperty()
   imageLink: string;
+
   @ApiProperty()
   fn: string;
+
   @ApiProperty()
   fp: string;
+
   @ApiProperty()
   amount: string;
+
   @ApiProperty()
   purpose: string;
+
   @ApiProperty()
   paidAt: Date;
+
   @ApiProperty()
   createdAt: Date;
+
   @ApiProperty()
   updatedAt: Date;
 }
